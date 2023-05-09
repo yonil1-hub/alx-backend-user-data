8db3cf7bc9616c4b4009d3d0c37157e1c17048ba
Jamescog <jamescog72@gmail.com>
2023-05-09 21:20:38 +0300

    make the code more pythonic with better docstring

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,6 +29,7 @@ class DB:
         don't exist,and sets the session object to None.
         """
         self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
